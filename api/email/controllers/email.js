5e41b19c9dc450c950fb5d72559a6346b565a399
WictorAW <wictor-a-w-1996@hotmail.com>
2022-03-21 15:42:39 +0900

    criando api de email

---
+++ api/email/controllers/email.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {parseMultipartData, sanitizeEntity} = require('strapi-utils')
+
+module.exports = {
+
+    async create(ctx) {
+        let entity;
+
+        if(ctx.is('multipart')){
+            const {data, files} = parseMultipartData(ctx);
+
+            entity = await strapi.services.email.create(data, {files});
+
+        }else {
+            entity = await strapi.services.email.create(ctx.request.body);
+
+        }
+        
+        return sanitizeEntity(entity, {model: strapi.models.email})
+    }
+};
