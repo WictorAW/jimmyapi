90ba3a770af3bdeb6fdbd2df2637487c17481ee7
WictorAW <wictor-a-w-1996@hotmail.com>
2022-03-25 21:24:20 +0900

    fazendo essa porra funcionar

--- plugins/wysiwyg/admin/src/components/CKEditor/index.js
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CKEditor from '@ckeditor/ckeditor5-react';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
 import styled from 'styled-components';
 import ClassicEditor from "ckeditor5-build-strapi-wysiwyg";
 
@@ -45,12 +45,16 @@ const configuration = {
 };
 
 const Editor = ({ onChange, name, value }) => {
+  
   return (
     <Wrapper>
       <CKEditor
         editor={ClassicEditor}
         config={configuration}
         data={value}
+        onReady={(editor) => {
+          editor.setData(value);
+        }}
         onChange={(event, editor) => {
           const data = editor.getData();
           onChange({ target: { name, value: data } });
